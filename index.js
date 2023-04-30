d0b536b86fb188b1efdc8c07d9483ad79d4538f0
Dmitro Loshik <dmitroloshik@gmail.com>
2023-04-30 18:19:44 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const array = [1, 2, 3, 4, 5, 6, 7];
+function removeElement(arr, num) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === num) {
+            arr.splice(i,1)
+            console.log(array)
+        }
+    }
+}
+removeElement(array, 5);
